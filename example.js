7a2b14e869daec60570778575c586460825707fb
naijun0403 <bluerex@mcna.xyz>
2021-07-29 15:42:02 +0900

    Update example.js

--- example.js
+++ example.js
@@ -1,2 +1,11 @@
 const { KakaoLinkClient } = require('./index');
 const Kakao = new KakaoLinkClient("apiKey", "url")
+
+Kakao.login("email", "password");
+
+Kakao.sendLink('roomName', {
+    template_id: 00000,
+    template_args: {
+
+    }
+}, 'custom')
