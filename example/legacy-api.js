0d7907adee5b4ed32973b94945d81372e09d6fe4
naijun0403 <bluerex@mcna.xyz>
2021-08-06 11:38:45 +0900

    add example

---
+++ example/legacy-api.js
@@ -0,0 +1,19 @@
+/**
+ * Created by archethic on 2021/08/06
+ */
+
+const { KakaoLinkClient } = require('kakaolink');
+const Kakao = new KakaoLinkClient('jsKey', 'url');
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    if(msg === '!카카오링크') {
+        Kakao.sendLink('roomName', {
+            template_id: 00000,
+            template_args: {
+        
+            }
+        }, 'custom')
+    }
+}
+
+const logoutKaling = () => Kakao.logout(); //please don't use this
