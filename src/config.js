042189ec5776ba7cb0683ffa1d3bd777133fb7d5
naijun0403 <65700898+archethic@users.noreply.github.com>
2021-12-21 18:57:53 +0900

    #11 bug fixed?

--- src/config.js
+++ src/config.js
@@ -6,7 +6,20 @@
 
 exports.BasicConfig = {
     getLoginUrl: "https://sharer.kakao.com/talk/friends/picker/link",
-    getTiaraUrl: "https://stat.tiara.kakao.com/track?d=" + encodeURIComponent(JSON.stringify({"sdk":{"type":"WEB","version":"1.1.15"}})),
+    getTiaraUrl: "https://stat.tiara.kakao.com/track?d=" + encodeURIComponent(JSON.stringify({
+        sdk: { type: 'WEB', version: '1.1.17' },
+        env: { screen: '1920X1080', tz: '+9', cke: 'Y' },
+        common: {
+          svcdomain: 'accounts.kakao.com',
+          deployment: 'production',
+          url: 'https://accounts.kakao.com/login',
+          referrer: 'https://m.search.daum.net/',
+          title: '카카오계정',
+          section: 'login',
+          page: 'pageLogin'
+        },
+        action: { type: 'Pageview', name: 'pageLogin', kind: '' }
+      })),
     logoutUrl: "https://sharer.kakao.com/talk/friends/picker/logout",
     getAuthUrl: "https://accounts.kakao.com/weblogin/authenticate.json",
     getChatData: "https://sharer.kakao.com/api/talk/chats",
