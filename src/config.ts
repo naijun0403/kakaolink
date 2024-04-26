8b2ee5ae824e848535a8ac5fe1793955461dc9d7
naijun0403 <naijun0403@gmail.com>
2024-04-27 00:59:42 +0900

    feat: add config

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export interface Configuration {
+    defaultUserAgent: string;
+}
+
+export const config: Configuration = {
+    defaultUserAgent: 'Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+};
