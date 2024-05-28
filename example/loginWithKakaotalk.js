ac91adcd654d3d38621df1e21315a309867d5611
naijun0403 <naijun0403@gmail.com>
2024-05-28 23:33:31 +0900

    chore: add example

---
+++ example/loginWithKakaotalk.js
@@ -0,0 +1,18 @@
+var { KakaoApiService, KakaoShareClient } = require('kakaolink');
+
+const service = new KakaoApiService.createService();
+const client = new KakaoShareClient.createClient();
+
+/**
+ * 알아서 카카오톡으로 접속해 로그인 세션을 불러옴
+ */
+const cookies = service.login({
+    signInWithKakaoTalk: true
+}).awaitResult();
+
+client.init('js app key', 'web platform domain', cookies);
+
+client.sendLink('roomName', {
+    templateId: 1, // your template id
+    templateArgs: {},
+}, 'custom').awaitResult();
